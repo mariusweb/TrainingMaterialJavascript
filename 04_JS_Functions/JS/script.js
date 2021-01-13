e00cfd6c3ce14a74c8ea97b5be5034f527f31309
mariusweb <39734577+mariusweb@users.noreply.github.com>
2021-01-13 13:52:05 +0200

    04-2 corrected

--- 04_JS_Functions/JS/script.js
+++ 04_JS_Functions/JS/script.js
@@ -4,28 +4,18 @@
     let progresLine = document.querySelector(".progress-bar");
 
     let remuveAndSetAttribute = (setNumber) => {
-        let infoSuccess = ['bg-success', 'bg-info'];
-        let dangerSuccess = ['bg-success', 'bg-danger'];
-        let dangerInfo = ['bg-info', 'bg-danger'];
+        let infoSuccessDanger = ['bg-success', 'bg-info', 'bg-danger'];
         if(setNumber <= 39 && setNumber >= 0){
+            progresLine.classList.remove(...infoSuccessDanger);
             progresLine.classList.add('bg-danger');
-            if(progresLine.classList.contains(...infoSuccess)){
-                progresLine.classList.remove(...infoSuccess);
-                
-            }
             
         }else if(setNumber <= 69 && setNumber >= 40){
+            progresLine.classList.remove(...infoSuccessDanger);
             progresLine.classList.add('bg-info');
-            if(progresLine.classList.contains(...dangerSuccess)){
-                progresLine.classList.remove(...dangerSuccess);
-            }
             
         }else if(setNumber <= 100 && setNumber >= 70){
+            progresLine.classList.remove(...infoSuccessDanger);
             progresLine.classList.add('bg-success');
-            if(progresLine.classList.contains(...dangerInfo)){
-                progresLine.classList.remove(...dangerInfo);
-                
-            }
             
         }
         progresLine.removeAttribute('aria-valuenow');
@@ -35,9 +25,12 @@
 
     }
     let clickToSubmitProgres = (inputDiv) =>{
-        console.log(inputDiv.value);
         remuveAndSetAttribute(inputDiv.value);
     }
-    submitBtn.addEventListener('click', () => {clickToSubmitProgres(givenNumber)});
+    submitBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        clickToSubmitProgres(givenNumber);
+        givenNumber.value = "";
+    });
 })();
 
